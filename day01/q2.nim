e0d2fb0787313f856d6224cf62713b61d49bbf53
sk0g <animany@live.co.uk>
2023-12-01 17:48:17 +1000

    Tidy code

--- day01/q2.nim
+++ day01/q2.nim
@@ -2,38 +2,44 @@ import sequtils
 import std/strutils
 from strformat import fmt
 
-# PART 2
 proc getCalibrationValue2(input: string): int =
     # Dirty hacks: some numbers have their last character overlap
     # with the first character of their first letter.
     # Get around that by simply duplicating the final character for each number...
-    var correctedInput = input.multiReplace(
-        ("one", "onee"),
-        ("two", "twoo"),
-        ("three","threee"),
-        ("four", "fourr"),
-        ("five","fivee"),
-        ("six", "sixx"),
-        ("seven","sevenn"),
-        ("eight","eightt"),
-        ("nine","ninee")
-    )
-    correctedInput = correctedInput.multiReplace(
-        ("one", "1"),
-        ("two", "2"),
-        ("three", "3"),
-        ("four", "4"),
-        ("five", "5"),
-        ("six", "6"),
-        ("seven", "7"),
-        ("eight", "8"),
-        ("nine", "9"),
-    )
-    var digits = toSeq(correctedInput.items)
+    let fixedInput = input
+        .multiReplace(
+            ("one", "onee"),
+            ("two", "twoo"),
+            ("three","threee"),
+            ("four", "fourr"),
+            ("five","fivee"),
+            ("six", "sixx"),
+            ("seven","sevenn"),
+            ("eight","eightt"),
+            ("nine","ninee")
+        )
+        .multiReplace(
+            ("one", "1"),
+            ("two", "2"),
+            ("three", "3"),
+            ("four", "4"),
+            ("five", "5"),
+            ("six", "6"),
+            ("seven", "7"),
+            ("eight", "8"),
+            ("nine", "9"),
+        )
+    let digits = toSeq(fixedInput.items)
         .filterIt(it.isDigit)
         .mapIt(fmt"{it}".parseInt)
 
     return digits[0] * 10 + digits[digits.high]
+
+let values = readFile("q1.txt")
+    .splitLines
+    .toSeq
+    .map(getCalibrationValue2)
+echo "q2: ", values.foldl(a + b)
 
 
 assert "two1nine".getCalibrationValue2 == 29
@@ -47,12 +53,3 @@ assert "7pqrstsixteen".getCalibrationValue2 == 76
 assert "eighthree".getCalibrationValue2 == 83
 assert "sevenine".getCalibrationValue2 == 79
 assert "oneight".getCalibrationValue2 == 18
-
-proc q2() =
-    let values = readFile("q1.txt")
-        .splitLines
-        .toSeq
-        .map(getCalibrationValue2)
-    echo "q2: ", values.foldl(a + b)
-
-q2()
