ebe8acc5f3d506c108974904233a5d7ac7c55bea
sk0g <animany@live.co.uk>
2023-12-02 16:02:53 +1000

    Tidy asserts

--- day02/q1.nim
+++ day02/q1.nim
@@ -37,8 +37,13 @@ let idSum = readFile("q1.txt")
     .foldl(a + b)
 echo idSum
 
-assert readMinCubeCountsFromDraw("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green") == (1, (4, 2, 6))
-assert readMinCubeCountsFromDraw("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue") == (2, (1, 3, 4))
-assert readMinCubeCountsFromDraw("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red") == (3, (20, 13, 6))
-assert readMinCubeCountsFromDraw("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red") == (4, (14, 3, 15))
-assert readMinCubeCountsFromDraw("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green") == (5, (6, 3, 2))
+assert "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+    .readMinCubeCountsFromDraw() == (1, (4, 2, 6))
+assert "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"
+    .readMinCubeCountsFromDraw() == (2, (1, 3, 4))
+assert "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"
+    .readMinCubeCountsFromDraw() == (3, (20, 13, 6))
+assert "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"
+    .readMinCubeCountsFromDraw() == (4, (14, 3, 15))
+assert "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+    .readMinCubeCountsFromDraw() == (5, (6, 3, 2))
--- day02/q2.nim
+++ day02/q2.nim
@@ -14,8 +14,13 @@ let idSum = readFile("q1.txt")
 echo idSum
 
 
-assert readMinCubeCountsFromDraw("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")[1].calculatePower() == 48
-assert readMinCubeCountsFromDraw("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue")[1].calculatePower() == 12
-assert readMinCubeCountsFromDraw("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red")[1].calculatePower() == 1560
-assert readMinCubeCountsFromDraw("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red")[1].calculatePower() == 630
-assert readMinCubeCountsFromDraw("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green")[1].calculatePower() == 36
+assert "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+    .readMinCubeCountsFromDraw()[1].calculatePower() == 48
+assert "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"
+    .readMinCubeCountsFromDraw()[1].calculatePower() == 12
+assert "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"
+    .readMinCubeCountsFromDraw()[1].calculatePower() == 1560
+assert "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"
+    .readMinCubeCountsFromDraw()[1].calculatePower() == 630
+assert "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+    .readMinCubeCountsFromDraw()[1].calculatePower() == 36
